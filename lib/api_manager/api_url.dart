0249c21697883e2ee68a01529f1eb5f88659c7a8
saed <samersame404@gmail.com>
2023-10-25 22:04:49 +0300

    add ather marker

--- lib/api_manager/api_url.dart
+++ lib/api_manager/api_url.dart
@@ -1,26 +1,22 @@
 import 'package:map_package/api_manager/api_service.dart';
 
 import '../map/ui/widget/map_widget.dart';
+import 'dart:io';
 
 class PostUrl {
-  static final serverProxy =
-      '${constStartUrl}services/app/HttpRequestService/ExecuteRequest';
+  static const serverProxy =
+      'api/services/app/HttpRequestService/ExecuteRequest';
 }
 
 String get baseUrl {
-  return isAppleTestFromMapPackage ? 'proxy.cors.sh' : qareebBaseUrl;
+  if (Platform.isIOS) {
+    if (isAppleTestFromMapPackage) return testUrl;
+  }
+
+  return liveUrl;
 }
 
-String get qareebBaseUrl {
-  return isAppleTestFromMapPackage ? 'livetest.qareeb-maas.com' : 'live.qareeb-maas.com';
-}
+const liveUrl = 'live.qareeb-maas.com';
+const testUrl = 'qareeb-api.first-pioneers.com.tr';
 
-String get constStartUrl {
-  return isAppleTestFromMapPackage ? 'https://$qareebBaseUrl/api/' : 'api/';
-}
 
-// const baseUrlProxy = 'proxy.cors.sh';
-// const baseUrlQareeb = 'livetest.qareeb-maas.com';
-// const baseUrlQareebTest = 'live.qareeb-maas.com';
-
-// final baseUrl = 'livetest.qareeb-maas.com';
